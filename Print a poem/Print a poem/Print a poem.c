9f1726121c35b972590eb962c4658939b284ed5e
Bidiak Mykhailo <sgame666@gmail.com>
2021-02-03 23:31:09 +0200

    Added fist project Print a poem and gitignore

---
+++ Print a poem/Print a poem/Print a poem.c
@@ -0,0 +1,23 @@
+//Use the printf() statement to output a poem of your choice. Submit your program as a text file. 
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Would it be ok if I took some of your time?\n");
+    printf("Would it be ok if I wrote you a rhyme ?\n\n");
+    
+    printf("Would it be ok if I opened my heart ? \n");
+    printf("Would it be ok if I took on the part\n\n");
+
+    printf("Of being your manand showed you a view, \n");
+    printf("One that only a real man could do ? \n\n");
+
+    printf("Would it be ok if I could make you smile ? \n");
+    printf("Would it be ok if I held you awhile ? \n\n");
+
+    printf("Would it be ok if I kissed your face ? \n");
+    printf("Would it be ok if I were to replace\n\n");
+
+    return 0;
+}
